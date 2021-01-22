bd99d5c22901d68e965d15c7495e658e0a49ff34
dunnston <ryanscottdunn@gmail.com>
2021-01-22 04:50:42 +0000

    remove product name migrations

--- db/migrate/20210121230620_remove_product_name_from_sales.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductNameFromSales < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :sales, :product_name, :string
-  end
-end
--- db/migrate/20210122022236_remove_old_product_name_from_sales.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOldProductNameFromSales < ActiveRecord::Migration[6.1]
-  def change
-    remove_reference :sales, :product_name, null: false, foreign_key: true
-  end
-end
